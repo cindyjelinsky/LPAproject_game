7f26afe38f45e2fccdd3586982e7d7b6c1464f07
Cindy Jelinsky <cindyjelinsky@yahoo.com.br>
2024-06-15 11:49:26 -0300

    UML Diagram and Classes created

---
+++ GameClassDiagram/Entity.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from Enemy import Enemy
+
+class Entity(Enemy):
+    def __init__(self):
+        self.name = None
+        self.surf = None
+        self.rect = None
+
+    def move(self, ):
+        pass
+
---
+++ GameClassDiagram/Level.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Level:
+    def __init__(self):
+        self.window = None
+        self.entity_list = None
+        self.name = None
+
+    def run(self, ):
+        pass
+
+    def entity_list(self, ):
+        pass
+
